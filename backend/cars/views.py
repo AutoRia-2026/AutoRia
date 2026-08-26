02e07bc7b2f02ed71b99acea190c0365c036eac2
Ivan <Ivan@DESKTOP-4QS6SFC>
2026-08-26 17:49:12 +0300

    2

--- backend/cars/views.py
+++ backend/cars/views.py
@@ -1,13 +1,14 @@
 from rest_framework import viewsets
 
 from .models import Car
+from .permissions import IsOwnerOrReadOnly
 from .serializers import CarSerializer
 
 
 class CarViewSet(viewsets.ModelViewSet):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
+    permission_classes = [IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
-        owner = self.request.user if self.request.user.is_authenticated else None
-        serializer.save(owner=owner)
+        serializer.save(owner=self.request.user)
